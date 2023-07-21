c55b49ebb96cb1e07894c0753ba83c81ebb35ed4
RoshanellFrancisco <roshanellfrancisco@honeycomb.io>
2023-07-21 09:41:14 -0500

    updated tracing sheet

--- server/tracing.cjs
+++ server/tracing.cjs
@@ -14,38 +14,6 @@ const { NodeTracerProvider } = require ('@opentelemetry/sdk-trace-node')
 const { PrismaInstrumentation } = require ('@prisma/instrumentation')
 const { Resource } = require ( '@opentelemetry/resources')
 
-//prisma
-// const {
-//   SemanticResourceAttributes,
-// } = require("@opentelemetry/semantic-conventions");
-// const {
-//   OTLPTraceExporter,
-// } = require("@opentelemetry/exporter-trace-otlp-http");
-// const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-// const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-// const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-// const { PrismaInstrumentation } = require("@prisma/instrumentation");
-// const { Resource } = require("@opentelemetry/resources");
-
-// // Configure the trace provider
-// const provider = new NodeTracerProvider({
-//   resource: new Resource({
-//     [SemanticResourceAttributes.SERVICE_NAME]: 'example application',
-//   }),
-// })
-
-// // Configure how spans are processed and exported. In this case we're sending spans
-// // as we receive them to an OTLP-compatible collector (e.g. Jaeger).
-// provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()))
-
-// // Register your auto-instrumentors
-// registerInstrumentations({
-//   tracerProvider: provider,
-//   instrumentations: [new PrismaInstrumentation()],
-// })
-
-// // Register the provider globally
-// provider.register()
 // uses the HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
 const sdk = new HoneycombSDK({
     instrumentations: [ 
